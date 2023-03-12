a6ecc58c1235f210417533f7663583f5b18426e9
Davis Lang <s3775656@student.rmit.edu.au>
2023-03-12 23:23:20 +1100

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,27 @@
+/** @jsx h */
+
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "Davis' Blog",
+  description: "CREATIVE CODING!",
+  // header: <header>Your custom header</header>,
+  // section: <section>Your custom section</section>,
+  // footer: <footer>Your custom footer</footer>,
+  avatar: "./img/profilePic.jpg",
+  avatarClass: "rounded-full",
+  author: "Davis Lang",
+
+  // middlewares: [
+
+    // If you want to set up Google Analytics, paste your GA key here.
+    // ga("UA-XXXXXXXX-X"),
+
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    // redirects({
+    //  "/hello_world.html": "/hello_world",
+    // }),
+
+  // ]
+});
